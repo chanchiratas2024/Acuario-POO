a38488c102ebc83850637a3bf313ec70977d7467
Chura <jhonkalepc@Gmail.com>
2025-08-31 22:57:31 -0500

    Acuario: Herencia Terminado

---
+++ src/acuario/TanqueTorre.kt
@@ -0,0 +1,15 @@
+package acuario
+import kotlin.math.PI
+
+class TanqueTorre(override var alto: Int, var diametro: Int) :
+    Acuario(alto = alto, ancho = diametro, largo = diametro) {
+
+    override var volumen: Int
+        get() = ((ancho / 2.0) * (largo / 2.0) * alto / 1000.0 * PI).toInt()
+        set(valor) {
+            alto = ((valor * 1000.0 / PI) / ((ancho / 2.0) * (largo / 2.0))).toInt()
+        }
+
+    override var agua: Double = volumen * 0.8
+    override val forma: String = "cilindro"
+}
